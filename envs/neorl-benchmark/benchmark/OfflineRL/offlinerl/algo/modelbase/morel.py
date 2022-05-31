38eab42b12276f18652357a9be7c27dbc3acaf2e
Vladislav Kurenkov <>
2022-05-31 21:34:22 +0300

    [site] show your work

--- envs/neorl-benchmark/benchmark/OfflineRL/offlinerl/algo/modelbase/morel.py
+++
@@ -1,5 +0,0 @@
-# MOReL: Model-Based Offline Reinforcement Learning
-# https://arxiv.org/abs/2005.05951
-# https://github.com/aravindr93/mjrl/tree/v2/projects/morel
-
-# TODO
--- envs/neorl-benchmark/benchmark/OfflineRL/offlinerl/algo/online/bremen.py
+++
@@ -1,5 +0,0 @@
-# Deployment-Efficient Reinforcement Learning via Model-Based Offline Optimization
-# https://arxiv.org/abs/2006.03647
-# https://github.com/matsuolab/BREMEN
-
-# TODO
